c08e69b5d9cb5ba4901df4880b0480ef399610e9
Jeremiah Dost <dostjh@gmail.com>
2019-04-28 21:36:19 -0700

    Initial commit of dochelpmate

---
+++ DocHelpMate/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace DocHelpMate
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.SourceFileTextBox = new System.Windows.Forms.TextBox();
+			this.SourceFileButton = new System.Windows.Forms.Button();
+			this.SaveFileTextBox = new System.Windows.Forms.TextBox();
+			this.StartButton = new System.Windows.Forms.Button();
+			this.SaveFileButton = new System.Windows.Forms.Button();
+			this.SourceFileFieldName = new System.Windows.Forms.TextBox();
+			this.SaveFileFieldName = new System.Windows.Forms.TextBox();
+			this.InstructionBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// SourceFileTextBox
+			// 
+			this.SourceFileTextBox.Location = new System.Drawing.Point(112, 211);
+			this.SourceFileTextBox.Name = "SourceFileTextBox";
+			this.SourceFileTextBox.Size = new System.Drawing.Size(321, 20);
+			this.SourceFileTextBox.TabIndex = 0;
+			// 
+			// SourceFileButton
+			// 
+			this.SourceFileButton.Location = new System.Drawing.Point(439, 209);
+			this.SourceFileButton.Name = "SourceFileButton";
+			this.SourceFileButton.Size = new System.Drawing.Size(75, 23);
+			this.SourceFileButton.TabIndex = 1;
+			this.SourceFileButton.Text = "Set Source";
+			this.SourceFileButton.UseVisualStyleBackColor = true;
+			this.SourceFileButton.Click += new System.EventHandler(this.SourceFileButton_Click);
+			// 
+			// SaveFileTextBox
+			// 
+			this.SaveFileTextBox.Location = new System.Drawing.Point(112, 237);
+			this.SaveFileTextBox.Name = "SaveFileTextBox";
+			this.SaveFileTextBox.Size = new System.Drawing.Size(321, 20);
+			this.SaveFileTextBox.TabIndex = 2;
+			// 
+			// StartButton
+			// 
+			this.StartButton.Location = new System.Drawing.Point(439, 264);
+			this.StartButton.Name = "StartButton";
+			this.StartButton.Size = new System.Drawing.Size(75, 23);
+			this.StartButton.TabIndex = 3;
+			this.StartButton.Text = "Start";
+			this.StartButton.UseVisualStyleBackColor = true;
+			this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+			// 
+			// SaveFileButton
+			// 
+			this.SaveFileButton.Location = new System.Drawing.Point(439, 235);
+			this.SaveFileButton.Name = "SaveFileButton";
+			this.SaveFileButton.Size = new System.Drawing.Size(75, 23);
+			this.SaveFileButton.TabIndex = 4;
+			this.SaveFileButton.Text = "Set Folder";
+			this.SaveFileButton.UseVisualStyleBackColor = true;
+			this.SaveFileButton.Click += new System.EventHandler(this.SaveFileButton_Click);
+			// 
+			// SourceFileFieldName
+			// 
+			this.SourceFileFieldName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.SourceFileFieldName.Location = new System.Drawing.Point(11, 214);
+			this.SourceFileFieldName.Name = "SourceFileFieldName";
+			this.SourceFileFieldName.ReadOnly = true;
+			this.SourceFileFieldName.Size = new System.Drawing.Size(95, 13);
+			this.SourceFileFieldName.TabIndex = 5;
+			this.SourceFileFieldName.Text = "Source File";
+			// 
+			// SaveFileFieldName
+			// 
+			this.SaveFileFieldName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.SaveFileFieldName.Location = new System.Drawing.Point(11, 240);
+			this.SaveFileFieldName.Name = "SaveFileFieldName";
+			this.SaveFileFieldName.ReadOnly = true;
+			this.SaveFileFieldName.Size = new System.Drawing.Size(95, 13);
+			this.SaveFileFieldName.TabIndex = 6;
+			this.SaveFileFieldName.Text = "Destination Folder";
+			// 
+			// InstructionBox
+			// 
+			this.InstructionBox.Location = new System.Drawing.Point(13, 11);
+			this.InstructionBox.Multiline = true;
+			this.InstructionBox.Name = "InstructionBox";
+			this.InstructionBox.ReadOnly = true;
+			this.InstructionBox.Size = new System.Drawing.Size(501, 191);
+			this.InstructionBox.TabIndex = 7;
+			this.InstructionBox.Text = resources.GetString("InstructionBox.Text");
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(526, 299);
+			this.Controls.Add(this.InstructionBox);
+			this.Controls.Add(this.SaveFileFieldName);
+			this.Controls.Add(this.SourceFileFieldName);
+			this.Controls.Add(this.SaveFileButton);
+			this.Controls.Add(this.StartButton);
+			this.Controls.Add(this.SaveFileTextBox);
+			this.Controls.Add(this.SourceFileButton);
+			this.Controls.Add(this.SourceFileTextBox);
+			this.Name = "Form1";
+			this.Text = "PDF to Word Document Helper";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox SourceFileTextBox;
+		private System.Windows.Forms.Button SourceFileButton;
+		private System.Windows.Forms.TextBox SaveFileTextBox;
+		private System.Windows.Forms.Button StartButton;
+		private System.Windows.Forms.Button SaveFileButton;
+		private System.Windows.Forms.TextBox SourceFileFieldName;
+		private System.Windows.Forms.TextBox SaveFileFieldName;
+		private System.Windows.Forms.TextBox InstructionBox;
+	}
+}
+
---
+++ DocHelpMate/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace DocHelpMate
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void StartButton_Click(object sender, EventArgs e)
+		{
+			if (SourceFileTextBox.Text == "" || SaveFileTextBox.Text == "")
+			{
+				MessageBox.Show("You need to specify a source file and folder to save to.", "Files not specified");
+				return;
+			}
+			FormatDoc();
+		}
+
+		private void FormatDoc()
+		{
+			object missing = System.Reflection.Missing.Value;
+			object readOnly = false;
+
+			// object fileName = @"C:\Users\Jeremiah\Documents\Visual Studio 2017\Projects\DocHelpMate\Chapter II Aircraft Engine Types And Construction.docx";
+			object fileName = SourceFileTextBox.Text;
+			//object outputFileName = @"C:\Users\Jeremiah\Documents\Visual Studio 2017\Projects\DocHelpMate\Chapter II Aircraft Engine Types And Construction_Formatted.docx";
+			string sourceFileName = SourceFileTextBox.Text.Split('\\').Last();
+			string sourceFileNameLessExt = sourceFileName.Split('.')[sourceFileName.Split('.').Count() - 2];
+			object outputFileName = SaveFileTextBox.Text + "\\" + sourceFileNameLessExt + string.Format("_autoformatted_{0}.docx",DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+
+			Word.Application word = new Word.Application();
+			word.Visible = false;
+			Word.Document doc = word.Documents.Open(ref fileName);
+
+			doc.Activate();
+
+			// Resave the file so we don't touch the original
+			doc.SaveAs2(ref outputFileName);
+
+			// Create the find/replace object
+			Word.Find findObject = doc.Content.Find;
+			object replaceAll = Word.WdReplace.wdReplaceAll;
+
+			// Eliminate columns
+			doc.PageSetup.TextColumns.SetCount(1);
+			FindAndReplace("^n", "", ref findObject, missing, replaceAll);
+
+			// Eliminate section breaks
+			FindAndReplace("^b", "", ref findObject, missing, replaceAll);
+			
+			// Set all text to Body Text style
+			object baseStyle = "Body Text";
+			doc.Content.set_Style(ref baseStyle);
+			doc.Content.Font.Name = "Times New Roman";
+			doc.Content.ParagraphFormat.FirstLineIndent = 25; // Pixels
+			doc.Content.ParagraphFormat.LineUnitBefore = 0.5f; // Line based on 12 pt font
+
+			/*
+			 * Originally, we wanted to replace these shapes with stylized text. However,
+			 * it is proving difficult to do this accurately. Instead, we are opting for 
+			 * removing the images, and the editor will add text as appropriate to indicate
+			 * where an image was. In most cases, this will be fine since these images
+			 * are usually captioned with "Figure x-x. Description of figure."
+			 * 
+			 * We attempted the original spec by creating  alist of the ranges, and adding
+			 * to that list as we found shapes. However, for generic shapes, which were
+			 * not captured by the inlineshape type, we needed to grab the anchor. Using
+			 * the generic shape type made things difficult for two reasons:
+			 *   1. We would get multiple shapes in the same place.
+			 *   2. We would get the shape's anchor, which was not its actual position.
+			 * 
+			 * To overcome the first issue, we tried to only capture shapes on the first
+			 * deletion pass, but that didn't work.
+			 * 
+			 * The second issue resulted in the unreliability of the placement of the images.
+			 * 
+			 */
+
+			// Delete all images and tables
+
+			var tables = doc.Tables;
+			var shapes = doc.Shapes;
+			var images = doc.InlineShapes;
+
+			foreach (Word.Table table in tables)
+			{
+				table.Delete();
+			}
+
+			// For anchored shapes, we need to make multiple passes to actually hit them all.
+			while (shapes.Count > 0)
+			{
+				foreach (Word.Shape shape in shapes)
+				{
+					shape.Delete();
+				}
+				shapes = doc.Shapes;
+			}
+
+			foreach (Word.InlineShape iShape in images)
+			{
+				if (iShape.Type == Word.WdInlineShapeType.wdInlineShapePicture)
+				{
+					iShape.Delete();
+				}
+			}
+
+			/*
+			 * Character Clean Up!
+			 * 
+			 * Here, we're looking for common OCR problems and fixing them
+			 * with something cleaner.
+			 * 
+			 */
+
+			var textFound = false;
+
+			// Paragraph breaks in a row
+			textFound = true;
+			while (textFound == true)
+			{
+				textFound = FindAndReplace("^p^p", "^p", ref findObject, missing, replaceAll);
+				// TODO: Just delete every paragraph that has no text after trimming.
+				// We need to do this because the FindAndReplace will not replace the last paragraph of a doc.
+				if (doc.Paragraphs.Last.Range.Text.Trim() == string.Empty)
+				{
+					doc.Paragraphs.Last.Range.Delete();
+				}
+			}
+
+			// Multiple spaces in a row
+			textFound = true;
+			while (textFound == true)
+			{
+				textFound = FindAndReplace("  ", " ", ref findObject, missing, replaceAll);
+			}
+
+			// Spaces after an optional hyphen
+			textFound = true;
+			while (textFound == true)
+			{
+				// This line is deceptive. There is a soft-hyphen character here that
+				// appears invisible. However, I'm unable to determine a way to get this
+				// character to appear or otherwise represent it.
+				textFound = FindAndReplace("­ ", "", ref findObject, missing, replaceAll);
+			}
+
+			// Spaces after an optional hyphen
+			textFound = true;
+			while (textFound == true)
+			{
+				textFound = FindAndReplace("- ", "", ref findObject, missing, replaceAll);
+			}
+
+			// Multiple spaces before a period
+			textFound = true;
+			while (textFound == true)
+			{
+				textFound = FindAndReplace(" .", ".", ref findObject, missing, replaceAll);
+			}
+
+			// Multiple spaces before a comma
+			textFound = true;
+			while (textFound == true)
+			{
+				textFound = FindAndReplace(" ,", ",", ref findObject, missing, replaceAll);
+			}
+
+			// Save the doc!
+			doc.Save();
+			word.Visible = true;
+		}
+
+		private bool FindAndReplace(string f, string r, ref Word.Find findObject, object missing, object replaceAll)
+		{
+			findObject.ClearFormatting();
+			findObject.Replacement.ClearFormatting();
+			findObject.Text = f;
+			findObject.Replacement.Text = r;
+			var foundTextToReplace = findObject.Execute(
+				ref missing, ref missing, ref missing, ref missing, ref missing,
+				ref missing, ref missing, ref missing, ref missing, ref missing,
+				replaceAll, ref missing, ref missing, ref missing, ref missing
+				);
+
+			return foundTextToReplace;
+		}
+
+		private void SaveFileButton_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				SaveFileTextBox.Text = dialog.SelectedPath;
+			}
+
+		}
+
+		private void SourceFileButton_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Filter = "doc files (*.doc,*.docx)|*.doc;*.docx|All files (*.*)|*.*";
+
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				SourceFileTextBox.Text = dialog.FileName;
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
